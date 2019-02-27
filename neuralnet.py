c5da498049f08961735c8dc59c6fe363f6cf4207
tim-laeufer <laeufertim@gmail.com>
2019-02-27 11:23:04 +0100

    removed unused code, still no speedup

--- neuralnet.py
+++ neuralnet.py
@@ -38,234 +38,6 @@ class neuralnet:
         self.outputlayer_size = topology[2]
         self.create_arrays()
         #self.print_net()
-    
-    @property
-    def learning_rate(self):
-        return self.__learning_rate
-    
-    @learning_rate.setter
-    def learning_rate(self, learning_rate):
-        self.__learning_rate = learning_rate
-    
-    @learning_rate.getter
-    def learning_rate(self):
-        return self.__learning_rate
-    
-    @property
-    def inertia(self):
-        return self.__inertia
-    
-    @inertia.setter
-    def inertia(self, inertia):
-        self.__inertia = inertia
-    
-    @inertia.getter
-    def inertia(self):
-        return self.__inertia
-    
-    @property
-    def inputlayer_size(self):
-        return self.__inputlayer_size
-    
-    @inputlayer_size.setter
-    def inputlayer_size(self, inputlayer_size):
-        self.__inputlayer_size = inputlayer_size
-    
-    @inputlayer_size.getter
-    def inputlayer_size(self):
-        return self.__inputlayer_size
-    
-    @property
-    def hiddenlayer_size(self):
-        return self.__hiddenlayer_size
-    
-    @hiddenlayer_size.setter
-    def hiddenlayer_size(self, hiddenlayer_size):
-        self.__hiddenlayer_size = hiddenlayer_size
-    
-    @hiddenlayer_size.getter
-    def hiddenlayer_size(self):
-        return self.__hiddenlayer_size
-    
-    @property
-    def outputlayer_size(self):
-        return self.__outputlayer_size
-    
-    @outputlayer_size.setter
-    def outputlayer_size(self, outputlayer_size):
-        self.__outputlayer_size = outputlayer_size
-    
-    @outputlayer_size.getter
-    def outputlayer_size(self):
-        return self.__outputlayer_size
-    
-    @property
-    def inputlayer_input(self):
-        return self.__inputlayer_input
-    
-    @inputlayer_input.setter
-    def inputlayer_input(self, inputlayer_input):
-        self.__inputlayer_input = inputlayer_input
-    
-    @inputlayer_input.getter
-    def inputlayer_input(self):
-        return self.__inputlayer_input
-    
-    @property
-    def hiddenlayer_input(self):
-        return self.__hiddenlayer_input
-    
-    @property
-    def inputlayer_output(self):
-        return self.__inputlayer_output
-    
-    @inputlayer_output.setter
-    def inputlayer_output(self, inputlayer_output):
-        self.__inputlayer_output = inputlayer_output
-    
-    @inputlayer_output.getter
-    def inputlayer_output(self):
-        return self.__inputlayer_output
-    
-    @hiddenlayer_input.setter
-    def hiddenlayer_input(self, hiddenlayer_input):
-        self.__hiddenlayer_input = hiddenlayer_input
-    
-    @hiddenlayer_input.getter
-    def hiddenlayer_input(self):
-        return self.__hiddenlayer_input
-    
-    @property
-    def hiddenlayer_output(self):
-        return self.__hiddenlayer_output
-    
-    @hiddenlayer_output.setter
-    def hiddenlayer_output(self, hiddenlayer_output):
-        self.__hiddenlayer_output = hiddenlayer_output
-    
-    @hiddenlayer_output.getter
-    def hiddenlayer_output(self):
-        return self.__hiddenlayer_output
-    
-    @property
-    def outputlayer_input(self):
-        return self.__outputlayer_input
-    
-    @outputlayer_input.setter
-    def outputlayer_input(self, outputlayer_input):
-        self.__outputlayer_input = outputlayer_input
-    
-    @outputlayer_input.getter
-    def outputlayer_input(self):
-        return self.__outputlayer_input
-    
-    @property
-    def outputlayer_output(self):
-        return self.__outputlayer_output
-    
-    @outputlayer_output.setter
-    def outputlayer_output(self, outputlayer_output):
-        self.__outputlayer_output = outputlayer_output
-    
-    @outputlayer_output.getter
-    def outputlayer_output(self):
-        return self.__outputlayer_output
-    
-    @property
-    def inputlayer_weights(self):
-        return self.__inputlayer_weights
-    
-    @inputlayer_weights.setter
-    def inputlayer_weights(self, inputlayer_weights):
-        self.__inputlayer_weights = inputlayer_weights
-    
-    @inputlayer_weights.getter
-    def inputlayer_weights(self):
-        return self.__inputlayer_weights
-    
-    @property
-    def hiddenlayer_weights(self):
-        return self.__hiddenlayer_weights
-    
-    @hiddenlayer_weights.setter
-    def hiddenlayer_weights(self, hiddenlayer_weights):
-        self.__hiddenlayer_weights = hiddenlayer_weights
-    
-    @hiddenlayer_weights.getter
-    def hiddenlayer_weights(self):
-        return self.__hiddenlayer_weights
-    
-    @property
-    def old_inputlayer_weights(self):
-        return self.__old_inputlayer_weights
-    
-    @old_inputlayer_weights.setter
-    def old_inputlayer_weights(self, old_inputlayer_weights):
-        self.__old_inputlayer_weights = old_inputlayer_weights
-    
-    @old_inputlayer_weights.getter
-    def old_inputlayer_weights(self):
-        return self.__old_inputlayer_weights
-    
-    @property
-    def old_hiddenlayer_weights(self):
-        return self.__old_hiddenlayer_weights
-    
-    @old_hiddenlayer_weights.setter
-    def old_hiddenlayer_weights(self, old_hiddenlayer_weights):
-        self.__old_hiddenlayer_weights = old_hiddenlayer_weights
-    
-    @old_hiddenlayer_weights.getter
-    def old_hiddenlayer_weights(self):
-        return self.__old_hiddenlayer_weights
-    
-    @property
-    def bias(self):
-        return self.__bias
-    
-    @bias.setter
-    def bias(self, bias):
-        self.__bias = bias
-    
-    @bias.getter
-    def bias(self):
-        return self.__bias
-    
-    @property
-    def alpha(self):
-        return self.__alpha
-    
-    @alpha.setter
-    def alpha(self, alpha):
-        self.__alpha = alpha
-    
-    @alpha.getter
-    def alpha(self):
-        return self.__alpha
-    
-    @property
-    def images(self):
-        return self.__images
-    
-    @images.setter
-    def images(self, images):
-        self.__images = images
-    
-    @images.getter
-    def images(self):
-        return self.__images
-    
-    @property
-    def labels(self):
-        return self.__labels
-    
-    @labels.setter
-    def labels(self, labels):
-        self.__labels = labels
-    
-    @labels.getter
-    def labels(self):
-        return self.__labels
         
     #Math functions for the neurons:
     def ac_fct(self, x):
@@ -363,23 +135,22 @@ class neuralnet:
                 self.inputlayer_weights[j, i] = new_w
     
     def calc_output_gradient(self, i_out, j_hid, expected):
-        a = self.ac_fct(self.hiddenlayer_input[j_hid] - self.bias)
-        my_inp = self.outputlayer_input[i_out]
-        v = 2*self.learning_rate*(expected-self.ac_fct(my_inp))*self.der_fct(
-                my_inp) * a
+        v = 2*self.learning_rate*(expected-self.ac_fct(
+                self.outputlayer_input[i_out]))*self.der_fct(
+                self.outputlayer_input[i_out]) * self.ac_fct(
+                        self.hiddenlayer_input[j_hid] - self.bias)
         return v
     
     def calc_hidden_gradient(self, i_hid, j_inp, expect_arr):
         s = 0.0
         for k in range(self.outputlayer_size):
-            ex = expect_arr[k]
-            s+=ex - self.ac_fct(self.outputlayer_input[k])
+            s+=expect_arr[k] - self.ac_fct(self.outputlayer_input[k])
             s*=self.der_fct(self.outputlayer_input[k])
             s*=self.hiddenlayer_weights[i_hid, k]
             
-        a = self.ac_fct(self.inputlayer_input[j_inp] - self.bias)
         return (2*self.learning_rate*s*self.der_fct(
-                self.hiddenlayer_input[i_hid])*a)
+                self.hiddenlayer_input[i_hid])*self.ac_fct(
+                        self.inputlayer_input[j_inp] - self.bias))
         
     def save_net(self, filename):
         with open(filename) as f:
